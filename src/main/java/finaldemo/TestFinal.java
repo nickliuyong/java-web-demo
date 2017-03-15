a7013184ed6a1f4f21bfa21cdec449749471a913
liuyong <liuyong@zjlp.com>
2017-03-15 09:22:58 +0800

    增加final关键字理解

---
+++ src/main/java/finaldemo/TestFinal.java
@@ -0,0 +1,12 @@
+package finaldemo;
+
+/**
+ * final关键字修饰变量，作用域内不允许改变其值或引用地址
+ * @author LiuYong on 2017/3/14 0014 17:30.
+ */
+public class TestFinal {
+
+    public static void main(String[] args){
+
+    }
+}
